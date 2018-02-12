a854637a39eb4545b34523bffef1e1a686243ae7
Christoffer G. Thomsen <chris@cgt.name>
2018-02-12 13:59:48 +0100

    Add StudentApp consumer

---
+++ StudentApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentApp.StudentService;
+
+namespace StudentApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new StudentServiceClient())
+            {
+                var students = client.GetAllStudents();
+                foreach (var s in students)
+                {
+                    Console.WriteLine($"{s.Id}\t{s.Name}");
+                }
+            }
+        }
+    }
+}
